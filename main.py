ecbe9f9f16e7ebcea7ee5db0d0a7b2a959db3c62
Kellphy <virtual_44@yahoo.com>
2024-05-12 13:53:53 +0300

    restore logic

--- main.py
+++ main.py
@@ -518,8 +518,8 @@ def main() -> None:
         pot_winning = pot_winnings(s, header)
 
         # Checks if the user wants to claim the lucky pot and do so if the pot isn't claimed yet.
-        #if settings['lucky_pot'] and pot_winning['data']['winning_credits'] is None:
-        pot_claim(s, header)
+        if settings['lucky_pot'] and pot_winning['data']['winning_credits'] is None:
+            pot_claim(s, header)
 
         if not referrals_claim(s, header):
             logging.error('%sFailed to claim referrals.', RED)
